0924157003bee7f82bc627eb94873ba12f695bb3
zen <contact.zenreportz@gmail.com>
2020-09-12 13:50:21 -0600

    added python vesion

---
+++ python-package/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Zen Publish", # Replace with your own username
+    version="0.0.1",
+    author="Dr. P. B. Patel",
+    author_email="contact.zenreportz@gmail.com",
+    description="Package for Zen Reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: APACHE2 License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'click', 'pyyaml', 'click', 'requests'
+    ],
+    python_requires='>=3',
+    entry_points='''
+        [console_scripts]
+        zen=main:zen
+    ''',
+)
